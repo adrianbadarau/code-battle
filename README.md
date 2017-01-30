ef03dec2222cd994bc7f82f3957308e3e8dadd95
Adrian Badarau <adi.badarau@yahoo.com>
2017-01-30 22:50:42 +0200

    Project start - Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+.checkout
+=========
+
+A Symfony project refactored after the KNP University REST project to work with Symfony 3.
+
+This is a small game that allows users to create hero (programmers ) tat will fight epic bosses (projects)  in a battle to the death ... of logic
+
+Good place to learn REST and Symfony as it is a small and simple CRUD project
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Table(name="battle_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
